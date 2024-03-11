f5491d7af290fe87a2fab83a87e0c5fb6fb8f9a0
SelmanKarakas101 <selmankarakas1@gmail.com>
2024-03-12 00:22:04 +0300

    QuizApp - Questions

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO question (category, difficulty_level, right_answer, option4, option3, option2, option1, question_title) VALUES
+                                                                                                                        ('Java', 'Easy', 'option1', 'Option 4', 'Option 3', 'Option 2', 'Option 1', 'What is Java?'),
+                                                                                                                        ('Python', 'Medium', 'option3', 'Option D', 'Option C', 'Option B', 'Option A', 'What is Python?'),
+                                                                                                                        ('JavaScript', 'Hard', 'option2', 'Choice 4', 'Choice 3', 'Choice 2', 'Choice 1', 'What is JavaScript?'),
+                                                                                                                        ('C++', 'Easy', 'option4', 'Alternative 4', 'Alternative 3', 'Alternative 2', 'Alternative 1', 'What is C++?'),
+                                                                                                                        ('SQL', 'Medium', 'option1', 'Var 4', 'Var 3', 'Var 2', 'Var 1', 'What is SQL?');
